172744c2dfe61834475b5a896b45d2f5a0301b1d
Soveha <vatsoveha07@gmail.com>
2025-03-01 23:24:07 +0700

    Refactor routing structure to include protected access for the survey page

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -21,20 +21,21 @@ const Routers: React.FC = () => {
   return (
     <Router>
       <Routes>
+        {/* ----With Layout---- */}
         <Route element={<Layout />}>
           <Route path="/home" element={<Home />} />
         </Route>
-        <Route path="/survey" element={<PublicRoute><Survey /></PublicRoute>} />
+
         {/* ----Public Routes---- */}
         <Route path="/" element={<PublicRoute><Onboard /></PublicRoute>} />
         <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
         <Route path="/signup" element={<PublicRoute><SignUp /></PublicRoute>} />
-        
+
         {/* <Route path="/home" element={<PublicRoute><Home /></PublicRoute>} /> */}
         {/* ----End---- */}
 
         {/* ----Protected Routes---- */}
-        {/* <Route path="/home" element={<ProtectedRoute><Home /></ProtectedRoute>} /> */}
+        <Route path="/survey" element={<ProtectedRoute><Survey /></ProtectedRoute>} />
         {/* ----End---- */}
       </Routes>
     </Router>
